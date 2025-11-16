a55cfb7db7311be9c21d1888b5934f2dfa30dce4
Prefect Nine <prefect@uapp.ai>
2025-11-17 01:44:29 +0400

    Fix readme, styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./css/app.css"
 export default function App(){
     return (
         <div className="container">
-
+            Put your Select implementation here.
         </div>
     )
 }
